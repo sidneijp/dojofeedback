a9c070896ef4b0db272e2081057b54262a82ceb1
edgarlatorre <edgar.latorrer@gmail.com>
2012-07-05 20:07:04 -0300

    Removendo helloworld e adcionante templates
    Removi o arquivo helloworld.py
    Adicionei css, js e img
    Alterei a view inicial para salvar um dojo

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from models import Dojo
 
 views = Blueprint('views', __name__, static_folder='../static', template_folder='../templates')
 
 
 @views.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('dojo.html')
+
+@views.route('/dojo/create', methods=['POST'])
+def create():
+    dojo = Dojo(name=request.form['name'])
+    dojo.save()
+    return render_template('dojo.html', name=dojo.name) 

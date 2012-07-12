01e88049fb272c092c2497f7ae378f1679be55af
Edgar <edgar.romero@digitalcube.com.br>
2012-07-12 18:57:46 -0300

    Testando conexao heroku

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from views import views
 from mongoengine import connect
-import os
 
 
-connect(os.environ['MONGOHQ_URL'])
+connect('app5623357',
+    host='flame.mongohq.com',
+    port=27089,
+    username='dojofeedback',
+    password='dojofeedback2k11')
 
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 app.secret_key = 'dojofeedback'

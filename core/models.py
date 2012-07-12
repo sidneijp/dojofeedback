e7ca1a2b46d203c038cac06b2f62c5b8771503d8
Edgar <edgar.romero@digitalcube.com.br>
2012-07-12 20:24:06 -0300

    Revert "Revert "Terminei as views basicas para o funcionamento""
    This reverts commit 251ba78598dba3780545707ccb9bf5c2a0cda5d7.
    Revertendo a maldicao

--- core/models.py
+++ core/models.py
@@ -1,11 +1,35 @@
 from mongoengine import Document, EmbeddedDocument, StringField, ListField, EmbeddedDocumentField
+from mongoengine.queryset import InvalidQueryError
+from flask import abort
 
 
 class Comment(EmbeddedDocument):
     description = StringField(max_length=100, required=True)
+    status = StringField(max_length=100, required=True)
 
 
 class Dojo(Document):
     name = StringField(max_length=100, required=True)
     comments = ListField(EmbeddedDocumentField(Comment))
 
+    def positives(self):
+        positives = []
+        for comment in self.comments:
+            if comment.status == '1':
+                positives.append(comment)
+        return positives
+
+    def negatives(self):
+        negatives = []
+        for comment in self.comments:
+            if comment.status == '2':
+                negatives.append(comment)
+
+        return negatives
+
+    @classmethod
+    def get_or_404(cls, **kwargs):
+        try:
+            return cls.objects.get(**kwargs)
+        except (cls.MultipleObjectsReturned, cls.DoesNotExist, InvalidQueryError):
+            abort(404)

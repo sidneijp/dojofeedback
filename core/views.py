251ba78598dba3780545707ccb9bf5c2a0cda5d7
Edgar <edgar.romero@digitalcube.com.br>
2012-07-12 19:21:42 -0300

    Revert "Terminei as views basicas para o funcionamento"
    This reverts commit f592a9c6d34daecba4e9ff040676e07b0ffea08d.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, redirect
-from models import Dojo, Comment
+from flask import Blueprint, render_template, request
+from models import Dojo
 
 views = Blueprint('views', __name__, static_folder='../static', template_folder='../templates')
 
@@ -8,35 +8,8 @@ views = Blueprint('views', __name__, static_folder='../static', template_folder=
 def index():
     return render_template('dojo.html')
 
-
-@views.route('/dojo/<name>')
-def show(name):
-    dojo = Dojo.get_or_404(name=name)
-
-    return render_template('comentarios.html', dojo=dojo)
-
-
-@views.route('/dojo/<dojo_name>/feedback')
-def feedback(dojo_name):
-    dojo = Dojo.get_or_404(name=dojo_name)
-
-    return render_template('feedback.html', dojo=dojo)
-
-
 @views.route('/dojo/create', methods=['POST'])
 def create():
     dojo = Dojo(name=request.form['name'])
     dojo.save()
-
-    return redirect('/dojo/%s' % dojo.name)
-
-
-@views.route('/feedback/create', methods=['POST'])
-def create_feedback():
-    dojo = Dojo.get_or_404(id=request.form['dojo_id'])
-    comment = Comment(description=request.form['comment'],
-        status=request.form['status'])
-    dojo.comments.append(comment)
-    dojo.save()
-
-    return redirect('/dojo/%s' % dojo.name)
+    return render_template('dojo.html', name=dojo.name) 

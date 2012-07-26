c2a6b136c5c53e4d80e549712c9743f01f86e104
Edgar <edgar.romero@digitalcube.com.br>
2012-07-26 18:28:41 -0300

    Ajustando o init para conectar no mongo sem precisar configurar

--- core/__init__.py
+++ core/__init__.py
@@ -3,9 +3,12 @@ from views import views
 from mongoengine import connect
 import os
 
+try:
+    host=os.environ['MONGOHQ_URL']
+except KeyError:
+    host='localhost'
 
-#connect('app5623357', host=os.environ['MONGOHQ_URL'])
-connect('app5623357', host='localhost')
+connect('app5623357', host=host)
 
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 app.secret_key = 'dojofeedback'

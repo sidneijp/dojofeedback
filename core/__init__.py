9ec2cd17ad4b3cc9ce54cd816bc8994c2b2b18bf
Filipe Fagundes <filipe.fagundes@digitalcube.com.br>
2012-08-07 15:51:38 -0300

    Adicionadas tela de erro 404 e 500 e minificado o js

--- core/__init__.py
+++ core/__init__.py
@@ -1,18 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from views import views
 from mongoengine import connect
 import os
 
-try:
-    host=os.environ['MONGOHQ_URL']
-except KeyError:
-    host='localhost'
-
-connect('app5623357', host=host)
-
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 app.secret_key = 'dojofeedback'
 app.register_blueprint(views)
-app.debug = True
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def error(e):
+    return render_template('500.html'), 500
+
+try:
+    host=os.environ['MONGOHQ_URL']
+    app.debug = False
+except KeyError:
+    host='localhost'
+    app.debug = False
+
+connect('app5623357', host=host)
+
 if __name__ == "__main__":
     app.run()

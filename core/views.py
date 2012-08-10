9b429074659a1383b018f0107fe26ef163692f06
Renne Rocha <me@rennerocha.com>
2012-08-10 07:52:59 -0300

    Adicionando barras no roteamento das URL
    De acordo com a documentação do Flask (http://flask.pocoo.org/docs/quickstart/#variable-rules):
    
    Na modificação sugerida, o final das URLs possui uma barra no final. 
    Acessando elas sem a barra fará com que o Flask redirecione automaticamente para a URL com a barra no final.
    
    Da maneira que estava (sem a barra no final), acessando com a barra no final, produziria um erro
    404.
    
    Acredito que dessa maneira, fica mais difícil um usuário receber um erro 404 sem querer.

--- core/views.py
+++ core/views.py
@@ -12,21 +12,21 @@ def index():
     return render_template('dojo.html', form=form)
 
 
-@views.route('/dojo/<name>')
+@views.route('/dojo/<name>/')
 def comment(name):
     dojo = Dojo.get_or_404(name=name)
 
     return render_template('comentarios.html', dojo=dojo)
 
 
-@views.route('/dojo/<name>/feedback')
+@views.route('/dojo/<name>/feedback/')
 def feedback(name):
     dojo = Dojo.get_or_404(name=name)
 
     return render_template('feedback.html', dojo=dojo)
 
 
-@views.route('/dojo/create', methods=['POST'])
+@views.route('/dojo/create/', methods=['POST'])
 def create():
     response = {'success': False}
     form = DojoForm(request.form)
@@ -39,7 +39,7 @@ def create():
     return Response(simplejson.dumps(response), mimetype="application/json")
 
 
-@views.route('/feedback/create', methods=['POST'])
+@views.route('/feedback/create/', methods=['POST'])
 def create_feedback():
     dojo = Dojo.get_or_404(id=request.form['dojo_id'])
     comment = Comment(description=request.form['comment'],
